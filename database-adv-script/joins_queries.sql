2f24bc635a61408ceb78fc42d6d826aabd1f7ce0
Mohamed <119766769+Mohamed211814@users.noreply.github.com>
2025-07-11 17:19:30 +0100

    joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,4 +1,18 @@
-LEFT JOIN : Retrieve all properties and their reviews, including properties that have no reviews
+INNER JOIN: Retrieve all bookings and the users who made those bookings
+    
+SELECT 
+    bookings.booking_id,
+    bookings.property_id,
+    bookings.booking_date,
+    users.user_id,
+    users.name AS user_name,
+    users.email
+FROM bookings
+    
+INNER JOIN users ON bookings.user_id = users.user_id;
+
+
+LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
     
 SELECT 
     properties.property_id,
@@ -7,4 +21,32 @@ SELECT
     reviews.rating,
     reviews.comment
 FROM properties
+    
 LEFT JOIN reviews ON properties.property_id = reviews.property_id;
+
+
+FULL OUTER JOIN: Retrieve all users and all bookings, even if there’s no match
+NOTE: MySQL does not support FULL OUTER JOIN natively.
+The following workaround uses UNION of LEFT JOIN and RIGHT JOIN.
+
+SELECT 
+    users.user_id,
+    users.name AS user_name,
+    bookings.booking_id,
+    bookings.property_id,
+    bookings.booking_date
+FROM users
+    
+LEFT JOIN bookings ON users.user_id = bookings.user_id
+
+UNION
+
+SELECT 
+    users.user_id,
+    users.name AS user_name,
+    bookings.booking_id,
+    bookings.property_id,
+    bookings.booking_date
+FROM bookings
+    
+LEFT JOIN users ON bookings.user_id = users.user_id;
